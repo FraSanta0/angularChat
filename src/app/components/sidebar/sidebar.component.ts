c65b249615dbcf8c251ef0cdfff530ce78af0631
FraSanta0 <109066301+FraSanta0@users.noreply.github.com>
2022-08-05 11:48:36 +0200

    aggiunto il download della lista delle chat

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -10,14 +11,19 @@ import { Router } from '@angular/router';
 export class SidebarComponent implements OnInit {
 
   constructor(
-    private route : Router
+    private route : Router,
+    private http : HttpClient
   ) { }
   @Output() eventEmitter: EventEmitter<number> = new EventEmitter<number>();
 
   ngOnInit(): void {
+    this.downloadListachat();
   }
 
   userName="provaMittente";
+  listachatUrl=
+  "http://santaniellofrancesco.altervista.org/angularChat/api/listachat/readListachatAll.php";
+  listaChat : {ID_listachat:string, nome: string, dataCreazione:string, descrizione: string}[] = [];
 
   emitChat(id_chat: number){
     this.eventEmitter.emit(id_chat);
@@ -25,5 +31,12 @@ export class SidebarComponent implements OnInit {
 
   openAddChat(){
     this.route.navigateByUrl('/addChat');
+  }
+
+  downloadListachat(){
+    this.http.get<any>(this.listachatUrl).subscribe((response) => {
+      this.listaChat=response.body;
+      console.log(this.listaChat);
+    });
   }
 }
